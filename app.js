4cd5c19a85f1a0cdfc86b7ed84fe9422a48d231f
Faizan <faizan.mansur87@gmail.com>
2023-05-29 22:34:22 +0500

    Created new POST API for /tours

--- app.js
+++ app.js
@@ -6,15 +6,56 @@ const tours = JSON.parse(
 )
 
 const app = express()
+app.use(express.json()) // using middleware to get json body from requests
 
 const apiPrefix = '/api/v1/'
 
+// for getting tours
 app.get(`${apiPrefix}tours`, (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
       tours,
+    },
+  })
+})
+
+// for creating new tour
+app.post(`${apiPrefix}tours`, (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = { id: newId, ...req.body }
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      })
+    },
+  )
+})
+
+// for getting a particular tour
+app.get(`${apiPrefix}tours/:id`, (req, res) => {
+  const id = Number(req.params.id)
+  const tour = tours.find((tour) => tour.id === id)
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID!',
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
     },
   })
 })
