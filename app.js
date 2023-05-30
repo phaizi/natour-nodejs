89ed2f6a4d636f6d86d78eb7257f9aa35fc5e520
Faizan <faizan.mansur87@gmail.com>
2023-05-30 05:02:14 +0500

    Refactored routes and route handlers

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(express.json()) // using middleware to get json body from requests
 
 const apiPrefix = '/api/v1/'
 
-// for getting tours
-app.get(`${apiPrefix}tours`, (req, res) => {
+// route handlers
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -19,29 +19,9 @@ app.get(`${apiPrefix}tours`, (req, res) => {
       tours,
     },
   })
-})
+}
 
-// for creating new tour
-app.post(`${apiPrefix}tours`, (req, res) => {
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = { id: newId, ...req.body }
-  tours.push(newTour)
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      })
-    },
-  )
-})
-
-// for getting a particular tour
-app.get(`${apiPrefix}tours/:id`, (req, res) => {
+const getTour = (req, res) => {
   const id = Number(req.params.id)
   const tour = tours.find((tour) => tour.id === id)
 
@@ -58,12 +38,27 @@ app.get(`${apiPrefix}tours/:id`, (req, res) => {
       tour,
     },
   })
-})
+}
 
-// for updating a tour
-// but it does not work completely
-// as it will be implemented when we will connect the db
-app.patch(`${apiPrefix}tours/:id`, (req, res) => {
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = { id: newId, ...req.body }
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      })
+    },
+  )
+}
+
+const updateTour = (req, res) => {
   const id = Number(req.params.id)
   const tour = tours.find((tour) => tour.id === id)
 
@@ -80,9 +75,9 @@ app.patch(`${apiPrefix}tours/:id`, (req, res) => {
       tour: '<updated tour here>',
     },
   })
-})
+}
 
-app.delete(`${apiPrefix}tours/:id`, (req, res) => {
+const deleteTour = (req, res) => {
   const id = Number(req.params.id)
   const tour = tours.find((tour) => tour.id === id)
 
@@ -97,11 +92,19 @@ app.delete(`${apiPrefix}tours/:id`, (req, res) => {
     status: 'success',
     data: null,
   })
-})
+}
+
+// api routes
+app.route(`${apiPrefix}tours`).get(getAllTours).post(createTour)
+
+app
+  .route(`${apiPrefix}tours/:id`)
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour)
 
 const port = 3000
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
 })
-app.on
